747d2eb3e0c685600d26803b19db5bba28889857
dstark <gorokhov.dv93@gmail.com>
2020-09-29 15:03:48 +0300

    обновление файлов

--- src/all.js
+++ src/all.js
@@ -1,29 +1,77 @@
 const sendNewToDo = document.getElementById('sendNewToDo')
-newListToDo = document.getElementById('newListToDo')
-const allToDo = []
+const newListToDo = document.getElementById('newListToDo')
+const url = 'http://localhost:3000/todos'
+let allToDo = []
 
-function createAllToDo() {
-    const newToDo = document.createElement('li')
-    const newToDoName = document.createElement('input')
-    const confirmBtn = document.createElement('button')
+function addInput() {
+    let newToDo = document.createElement('li')
+    let newToDoName = document.createElement('input')
+    let confirmBtn = document.createElement('button')
 
-    confirmBtn.textContent = 'ok'
-    confirmBtn.onclick = () => {
-        if (newToDoName.value.length < 1) return
-        newToDo.textContent = newToDoName.value
-        const locBtn = document.createElement('button')
-        locBtn.textContent = 'go to todo'
-        locBtn.onclick = () => {
-            location = 'file:///Users/dmitrygorokhov/Desktop/Projects/ToDo%20js/all%20todo/index.html'
-        }
-        newToDo.appendChild(locBtn)
+    if (document.getElementById('newName')) {
+        return
     }
+    newToDoName.id = 'newName'
+    confirmBtn.textContent = 'ok'
+    confirmBtn.setAttribute('onclick', 'okBtn()')
+
     newToDo.appendChild(newToDoName)
     newToDo.appendChild(confirmBtn)
     newListToDo.appendChild(newToDo)
 }
-sendNewToDo.onclick = () => {
+function okBtn() {
+    let newName = document.getElementById('newName').value
+    if (newName.length < 1) {
+        return
+    }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ name: newName })
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        allToDo.push({
+            name: newName,
+            id: data.id
+        })
+        showList()
+        console.log(data);
+    });
+}
+function addNewNameToDo() {
+    let newName = document.getElementById('newName').value
     allToDo.push({
+        name: newName
     })
     createAllToDo()
+}
+function getToDos() {
+    fetch(url).then(response => {
+        return response.json()
+    }).then(data => {
+        allToDo = data
+        showList()
+        console.log(allToDo);
+    });
+}
+function openToDo(id) {
+    window.location.href = 'file:///Users/dmitrygorokhov/Desktop/Projects/ToDo_js/src/index.html?id=' + id
+}
+function showList() {
+    newListToDo.textContent = ''
+    allToDo.forEach(elem => {
+        let newLi = document.createElement('li')
+        let goTo = document.createElement('button')
+        newLi.textContent = elem.name
+        goTo.textContent = 'go to ToDo'
+        goTo.setAttribute('onclick', 'openToDo(' + elem.id + ')')
+        newLi.appendChild(goTo)
+        newListToDo.appendChild(newLi)
+    })
+}
+window.onload = function () {
+    getToDos()
 }
--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const btnSend = document.getElementsByClassName('btn-success')
 const listToDo = document.querySelector('.card-text')
 const btnRandom = document.getElementById('random')
 const btnSave = document.getElementById('save')
+const url = 'http://localhost:3000/ListTodos'
+let toDosId = null
 let toDo = []
-
+window.onload = function () {
+    getToDos()
+}
 function createList() {
     listToDo.textContent = ''
     toDo.forEach((elem, index) => {
@@ -15,9 +19,9 @@ function createList() {
         const spanText = document.createElement('span')
 
         newElem.className = 'list-group-item'
-        newElem.setAttribute('elem', index)
+        newElem.setAttribute('elem', elem.id)
 
-        spanText.setAttribute('elem', index)
+        spanText.setAttribute('elem', elem.id)
         spanText.id = 'span-' + index
         spanText.textContent = elem.name
 
@@ -28,14 +32,14 @@ function createList() {
         editElem.className = 'btn-outline-success'
         editElem.textContent = 'edit'
         editElem.style = 'margin-left: auto'
-        editElem.setAttribute('elem', index)
+        editElem.setAttribute('elem', elem.id)
         editElem.setAttribute('onclick', 'edit(' + index + ',this)')
 
         checkElem.className = 'check'
         checkElem.type = 'checkbox'
         checkElem.checked = elem.check
         checkElem.style = 'float: left'
-        checkElem.setAttribute('elem', index)
+        checkElem.setAttribute('elem', elem.id)
         if (elem.check) {
             spanText.classList.add('toggle')
         }
@@ -45,16 +49,38 @@ function createList() {
         newElem.appendChild(delElem)
         listToDo.appendChild(newElem)
 
-        delElem.addEventListener('click', () => {
-            toDo.splice(index, 1)
-            createList()
+        delElem.addEventListener('click', (e) => {
+            fetch(url + '/' + toDo[e.target.getAttribute('elem')].id, {
+                method: 'DELETE',
+            }).then(response => {
+                return response.json()
+            }).then(data => {
+                toDo.splice(e.target.getAttribute('elem'), 1)
+                createList()
+            }).catch(err => {
+                console.error(err)
+            })
         })
         checkElem.addEventListener('click', (e) => {
-            console.log(e.target);
-            toDo[e.target.getAttribute('elem')].check = e.target.checked
-            spanText.classList.toggle('toggle')
+            fetch(url + '/' + e.target.getAttribute('elem'), {
+                method: 'PATCH',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    check: e.target.checked
+                })
+            }).then(response => {
+                return response.json()
+            }).then(data => {
+                toDo[index].check = e.target.checked
+                spanText.classList.toggle('toggle')
+            }).catch(err => {
+                console.error(err)
+            })
         })
     })
+
 }
 btnRandom.addEventListener('click', () => {
     toDo = toDo.sort(function () {
@@ -64,24 +90,68 @@ btnRandom.addEventListener('click', () => {
 })
 btnSend[0].addEventListener('click', (e) => {
     e.preventDefault()
-    if ((!!input.value)) {
-        toDo.push({
+    if ((!input.value)) {
+        return
+    }
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
             name: input.value,
+            toDosId: toDosId,
             check: false,
         })
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        toDo.push({
+            name: input.value,
+            toDosId: toDosId,
+            check: false,
+            id: data.id
+        })
+        console.log(data);
         input.value = ''
         createList()
-    }
+    });
 })
 function edit(id, editElem) {
     let span = document.getElementById('span-' + id)
     if (span.getAttribute('contentEditable') == 'true') {
-        span.contentEditable = false
-        editElem.textContent = 'edit'
-        toDo[id].name = span.textContent
+
+        fetch(url + '/' + toDo[id].id, {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: span.textContent,
+            })
+        }).then(response => {
+            return response.json()
+        }).then(data => {
+            span.contentEditable = false
+            editElem.textContent = 'edit'
+            toDo[id].name = span.textContent
+        }).catch(err => {
+            console.error(err)
+        })
     }
     else {
-        span.contentEditable = true
-        editElem.textContent = 'save'
+        if (!toDo[id].check) {
+            span.contentEditable = true
+            editElem.textContent = 'save'
+        }
     }
-};
+}
+function getToDos() {
+    toDosId = Number(window.location.search.split('=')[1])
+    fetch(url + '?toDosId=' + toDosId).then(response => {
+        return response.json()
+    }).then(data => {
+        toDo = data
+        createList()
+    });
+}
